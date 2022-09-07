cdd3e84914c4e931bf571746ca55fab0e2628ca7
kermeade <kermeade@umich.edu>
2022-09-06 21:12:30 -0400

    Completed module

--- api/src/router.js
+++ api/src/router.js
@@ -1,6 +1,7 @@
 const { Router } = require('express');
 const multer = require('multer');
 const path = require('path');
+const imageProcessor = require('./imageProcessor');
 
 const router = Router();
 
@@ -29,11 +30,16 @@ const upload = multer({
     storage: storage
 });
 
-router.post('/upload', upload.single('photo'), (request, response) => {
+router.post('/upload', upload.single('photo'), async (request, response) => {
     if (request.fileValidationError){
         return response.status(400).json({error: request.fileValidationError});
     }
     else {
+        try {
+            await imageProcessor(request.file.filename);
+        } catch (error) {
+            
+        }
         return response.status(201).json({success: true});
     }
 });
